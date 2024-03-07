19f21ce9264ed2b7d0fd3db8e5b70488cccf029a
Kr-luis <kluis816@gmail.com>
2024-03-06 23:41:11 -0500

    Final pagina de perfil css

--- client/src/componentes/Perfil_Usuario.jsx
+++ client/src/componentes/Perfil_Usuario.jsx
@@ -15,46 +15,46 @@ const PerfilUsuario = () => {
         <div className="container_perfil">
           <div className="row">
             <div className="col-md-6" id="col1">
-              <h1>Perfil de Usuario</h1>
-              <img className="imagen_usuario" src={Foto} alt="Foto perfil" />
-              <h1 align="center">Kairos</h1>
-              <p align="center">Comentarios realizados: 37</p>
-              <hr></hr>
+              <h1 className="display-4">Perfil de Usuario</h1>
+              <img className="img-fluid imagen_usuario" src={Foto} alt="Foto perfil" />
+              <h1 className="text-center">Kairos</h1>
+              <p className="text-center">Comentarios realizados: 37</p>
+              <hr />
               <h1>Gustos</h1>
-              <ul>
-                <li>Musica</li>
-                <li>Arte</li>
-                <li>Terror</li>
-                <li>Comicon</li>
+              <ul className="list-group">
+                <li className="list-group-item">Musica</li>
+                <li className="list-group-item">Arte</li>
+                <li className="list-group-item">Terror</li>
+                <li className="list-group-item">Comicon</li>
               </ul>
-              <hr></hr>
+              <hr />
               <h1>Ultimos eventos</h1>
-              <ul>
-                <li>POLICALIFA</li>
-                <li>EL PIXEL</li>
+              <ul className="list-group">
+                <li className="list-group-item">POLICALIFA</li>
+                <li className="list-group-item">EL PIXEL</li>
               </ul>
-              <hr></hr>
+              <hr />
             </div>
             <div className="col-md-6" id="col2">
               <h1>Correo</h1>
               <p>ejemplo@correo.com</p>
-              <hr></hr>
+              <hr />
               <h1>Nombre</h1>
               <p>Pepe Garcia</p>
-              <hr></hr>
+              <hr />
               <h1>Eventos Asistidos</h1>
               <p>Dos eventos asistidos hasta ahora</p>
-              <hr></hr>
+              <hr />
               <h1>Calificaciones de eventos</h1>
               <p>Tu promedio de calificación es de 4.3 de 5</p>
-              <hr></hr>
+              <hr />
               <h1>Ubicaciones preferidas</h1>
-              <ul>
-                <li>Cumbaya</li>
-                <li>Quito</li>
-                <li>Cuenca</li>
+              <ul className="list-group">
+                <li className="list-group-item">Cumbaya</li>
+                <li className="list-group-item">Quito</li>
+                <li className="list-group-item">Cuenca</li>
               </ul>
-              <br></br>
+              <br />
               <nav>
                 <a className='perfil_a' href="/Historial">Historial</a>
               </nav>
